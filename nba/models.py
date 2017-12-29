eab31a2e4694013ddbc3306dcd908cd6582293ee
Brian <bliu9883@gmail.com>
2017-12-29 01:35:47 -0800

    Added player stats model, and changed update times for team, players, and stats

--- nba/models.py
+++ nba/models.py
@@ -1,18 +1,43 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
 class Team(models.Model):
 	team_name = models.CharField(max_length=200)
 	tri_code = models.CharField(max_length=200)
 	color = models.CharField(max_length=20, default="")
+	conference = models.CharField(max_length=30, default="")
+	division = models.CharField(max_length=10, default="")
+	updated_at = models.DateTimeField(auto_now=True)
 
 	def __str__(self):
 		return self.team_name
+		
+	def was_updated_recently(self):
+		return self.updated_at >= timezone.now() - datetime.timedelta(days=30)
 
 class Player(models.Model):
 	first_name = models.CharField(max_length=200, default="")
 	last_name = models.CharField(max_length=200, default="")
 	team = models.ForeignKey(Team, on_delete=models.CASCADE)
+	updated_at = models.DateTimeField(auto_now=True)
 
 	def __str__(self):
 		return self.first_name + " " + self.last_name
+		
+	def was_updated_recently(self):
+		return self.updated_at >= timezone.now() - datetime.timedelta(days=1)
+		
+class PlayerStats(models.Model):
+	player = models.ForeignKey(Player, on_delete=models.CASCADE)
+	points = models.CharField(max_length=10, default="0.0")
+	rebounds = models.CharField(max_length=10, default="0.0")
+	assists = models.CharField(max_length=10, default="0.0")
+	updated_at = models.DateTimeField(auto_now=True)
+	
+	def __str__(self):
+		return self.player
+		
+	def was_updated_recently(self):
+		return self.updated_at >= timezone.now() - datetime.timedelta(days=1)
--- nba/views.py
+++ nba/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import Team, Player
+from .models import Team, Player, PlayerStats
 import requests
 
 # Create your views here.
@@ -11,17 +11,27 @@ def get_all_nba_teams():
 	for i in r:
 		if i['isNBAFranchise']:
 			# nba_teams.append({'teamId': i['teamId'], 'fullName': i['fullName'], 'tricode': i['tricode']})		
-			team = Team(id=i['teamId'], team_name=i['fullName'], tri_code=i['tricode'])
-			team.save()
+			if not Team.objects.get(id=i['teamId']).was_updated_recently():
+				team = Team(id=i['teamId'], 
+							team_name=i['fullName'], 
+							tri_code=i['tricode'],
+							conference=i['confName'],
+							division=i['divName'])
+				team.save()
+			else:
+				break;
 
 def get_team_colors():
 	r = requests.get("http://data.nba.net/data/1h/prod/2017/teams_config.json")
 	r=r.json()['teams']['config']
 	for i in r:
 		if Team.objects.filter(id=i['teamId']).exists():
-			team = Team.objects.get(id=i['teamId'])
-			team.color = i['primaryColor']
-			team.save()
+			if not Team.objects.get(id=i['teamId']).was_updated_recently():
+				team = Team.objects.get(id=i['teamId'])
+				team.color = i['primaryColor']
+				team.save()
+			else:
+				break;
 	
 # start page
 def nba_teams(request):		
@@ -39,19 +49,56 @@ def get_players_on_team(teamId):
 	r = r.json()['league']['standard']
 	for i in r:
 		if str(teamId) == i['teamId']:
-			player = Player(id=i['personId'], 
-							team=Team.objects.get(id=teamId),
-							first_name=i['firstName'],
-							last_name=i['lastName'])
-			player.save()
-	
+			if Player.objects.filter(id=i['personId']).exists():
+				if not Player.objects.get(id=i['personId']).was_updated_recently():
+					player = Player(id=i['personId'], 
+									team=Team.objects.get(id=teamId),
+									first_name=i['firstName'],
+									last_name=i['lastName'])
+					player.save()
+				else:
+					break;
+			else:
+				player = Player(id=i['personId'], 
+									team=Team.objects.get(id=teamId),
+									first_name=i['firstName'],
+									last_name=i['lastName'])
+				player.save()
+def get_player_current_stats(teamId):
+	team = Team.objects.get(id=teamId)
+	players = team.player_set.all()
+	for i in players:
+		if PlayerStats.objects.filter(id=i.id).exists():
+			if not PlayerStats.objects.get(id=i.id).was_updated_recently():
+				r = requests.get("http://data.nba.net/data/10s/prod/v1/2017/players/" + str(i.id) + "_profile.json")
+				r = r.json()['league']['standard']['stats']['latest']
+				stats = PlayerStats(id=i.id, 
+									player=Player.objects.get(id=i.id),
+									points = r['ppg'],
+									rebounds = r['rpg'],
+									assists = r['apg'])
+				stats.save()
+			else:
+				break;
+		else:
+			r = requests.get("http://data.nba.net/data/10s/prod/v1/2017/players/" + str(i.id) + "_profile.json")
+			r = r.json()['league']['standard']['stats']['latest']
+			stats = PlayerStats(id=i.id, 
+								player=Player.objects.get(id=i.id),
+								points = r['ppg'],
+								rebounds = r['rpg'],
+								assists = r['apg'])
+			stats.save()
+
 # get team info using tricode as link
 def team_info(request, tri_code):
 	team = Team.objects.get(tri_code=tri_code)
 	get_players_on_team(team.id)
+	get_player_current_stats(team.id)
 	players = team.player_set.all().order_by('last_name')
 	template = loader.get_template('nba/team_roster.html')
 	context = {
+		'team': team,
 		'players': players,
 	}
 	return HttpResponse(template.render(context, request))
